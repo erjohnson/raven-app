a482b56e8f66f60d3d7664129ff34f775bcf02f3
Eric <erjohnson.pdx@gmail.com>
2014-09-10 10:58:02 -0700

    create tickets controller

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, :type => :model do
+
   it { should belong_to :user }
   it { should validate_presence_of :user_id }
   it { should validate_presence_of :question }
