d250662c93c46b507d129adb5f5d8896344e6347
Eric <erjohnson.pdx@gmail.com>
2014-09-10 11:02:23 -0700

    add tickets routes & views

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,44 @@
+class TicketsController < ApplicationController
+
+  def index
+    @tickets = Ticket.all
+  end
+
+  def new
+    @ticket = Ticket.new
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+    if @ticket.save
+      respond_to do |format|
+        format.html { redirect_to tickets_url }
+        format.js
+      end
+    end
+  end
+
+  def show
+    @ticket = Ticket.find(params[:id])
+  end
+
+  def edit
+    @ticket = Ticket.find(params[:id])
+  end
+
+  def update
+    @ticket = Ticket.find(params[:id])
+    @ticket.update(params[:ticket])
+    respond_to do |format|
+      format.html { redirect_to tickets_url }
+      format.js
+    end
+  end
+
+private
+
+  def ticket_params
+    params.require(:ticket).permit(:question, :lesson_id).merge(user_id: current_user.id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root to: "tickets#index"
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, controllers: { registrations: "users/registrations" }
+  resources :tickets
 end
