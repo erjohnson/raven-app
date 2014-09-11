4d62f6c14255c10fa9958f92e1bd921e32046974
Eric <erjohnson.pdx@gmail.com>
2014-09-11 09:58:22 -0700

    create lessons controller & routes

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,44 @@
+class LessonsController < ApplicationController
+
+  def index
+    @lessons = Lesson.all
+  end
+
+  def new
+    @lesson = Lesson.new
+  end
+
+  def create
+    @lesson = Lesson.new(lesson_params)
+    if @lesson.save
+      respond_to do |format|
+        format.html { redirect_to lessons_path }
+        format.js
+      end
+    end
+  end
+
+  def show
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def edit
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def update
+    @lesson = Lesson.find(params[:id])
+    @lesson.update(params[:lesson])
+    respond_to do |format|
+      format.html { redirect_to lessons_url }
+      format.js
+    end
+  end
+
+private
+
+  def lesson_params
+    params.require(:lesson).permit(:name, :section)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
   root to: "tickets#index"
+
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, controllers: { registrations: "users/registrations" }
   resources :tickets
+  resources :lessons
 end
