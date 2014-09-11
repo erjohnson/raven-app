d89e8b2cf8bf406feecbf6eafde5383912b49aae
Eric <erjohnson.pdx@gmail.com>
2014-09-11 15:13:19 -0700

    add show user view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "tickets#index"
 
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}, controllers: { registrations: "users/registrations" }
+  resources :users, only: [:show]
   resources :tickets
   resources :lessons
 end
