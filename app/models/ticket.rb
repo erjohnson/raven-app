4d8102d70d61659acfab10eecfcbcd71213743a2
Eric <erjohnson.pdx@gmail.com>
2014-09-13 13:55:20 -0700

    change styles

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,6 +4,8 @@ class Ticket < ActiveRecord::Base
 
   validates_presence_of :user_id, :lesson_id, :question
 
+  scope :open, -> { where(closed: false) }
+
   def take_ticket
     self.update(taken: true, taken_at: Time.new())
   end
