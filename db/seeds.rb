8b4523577856f8b700168393be8e061928dc4cf0
Eric <erjohnson.pdx@gmail.com>
2014-09-19 09:56:42 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Lesson.create(name: "Bus timetable", section: "Ruby on Rails (Conventional)", url: "http://www.learnhowtoprogram.com/lessons/bus-timetable")
+Lesson.create(name: "Hacker News clone", section: "Ruby on Rails (Conventional)", url: "http://www.learnhowtoprogram.com/lessons/hacker-news-clone")
+Lesson.create(name: "Photo sharing app", section: "Ruby on Rails (Conventional)", url: "http://www.learnhowtoprogram.com/lessons/photo-sharing-app")
+Lesson.create(name: "Stack Overflow clone", section: "Ruby on Rails (Ajax)", url: "http://www.learnhowtoprogram.com/lessons/stack-overflow-clone")
+Lesson.create(name: "Twitter clone", section: "Ruby on Rails (Ajax)", url: "http://www.learnhowtoprogram.com/lessons/twitter-clone")
+Lesson.create(name: "Airbnb clone", section: "Ruby on Rails (Ajax)", url: "http://www.learnhowtoprogram.com/lessons/airbnb-clone")
+
